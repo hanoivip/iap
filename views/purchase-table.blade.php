c163f17dea996162d0bf576ef2787daa33ee3039
unknown <game.oh.vn@gmail.com>
2020-02-21 17:28:47 +0700

    f

---
+++ views/purchase-table.blade.php
@@ -0,0 +1,9 @@
+@extends('hanoivip::layout') 
+
+@section('title', 'Pay with paypal')
+
+@section('content')
+
+<div id="my-purchase" data-svname="{{$svname}}" data-role="{{$role}}"></div>
+
+@endsection
