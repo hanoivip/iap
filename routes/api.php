a3cd84a6c2f478ad08d92b7bb226f8eeef73ba7c
wartune00 <wartune.mobi@gmail.com>
2020-06-03 13:54:33 +0700

    f

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['token'])->namespace('Hanoivip\Iap\Controllers')->prefix('api')->group(function () {
-    Route::get('/iap/items', 'GameController@getItems');
+    Route::any('/iap/items', 'GameController@getItems');
 });
 
 Route::namespace('Hanoivip\Iap\Controllers')->prefix('api')->group(function () {
-    Route::get('/iap/query', 'AdminController@query');
+    Route::any('/iap/query', 'AdminController@query');
 });
